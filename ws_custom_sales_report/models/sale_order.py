6858cfb128f5df22a2032211d7a2977270dd9393
Ljubomir <ljupcedavitkov14@gmail.com>
2024-11-28 12:56:24 +0100

    Added custom JSON report

--- ws_custom_sales_report/models/sale_order.py
+++ ws_custom_sales_report/models/sale_order.py
@@ -4,6 +4,7 @@ import xlsxwriter
 import csv
 import io
 import base64
+import json
 
 
 class SaleOrder(models.Model):
@@ -121,6 +122,63 @@ class SaleOrder(models.Model):
         )
 
         # Return the URL to the attachment
+        return {
+            "type": "ir.actions.act_url",
+            "url": "/web/content/%d?download=true" % attachment.id,
+            "target": "new",
+        }
+
+    def action_export_custom_json(self):
+        """Button action to trigger the JSON export"""
+        # Fetch sale order data
+        orders = self.search([("id", "=", self.id)])
+
+        # Prepare data to be serialized into JSON
+        order_data = []
+        for order in orders:
+            data = {
+                "order_name": order.name,
+                "customer": order.partner_id.name,
+                "order_date": (
+                    order.date_order.strftime("%Y-%m-%d") if order.date_order else ""
+                ),
+                "total_amount": order.amount_total,
+                "order_lines": [],
+            }
+            # Add line items
+            for line in order.order_line:
+                data["order_lines"].append(
+                    {
+                        "product_name": line.product_id.name,
+                        "quantity": line.product_uom_qty,
+                        "unit_price": line.price_unit,
+                        "line_total": line.price_subtotal,
+                    }
+                )
+            order_data.append(data)
+
+        # Convert Python dictionary to JSON string
+        json_content = json.dumps(order_data, indent=4)
+
+        # Create an in-memory file (StringIO)
+        file_data = io.BytesIO(json_content.encode("utf-8"))
+
+        # Base64 encode the file content
+        file_data_base64 = base64.b64encode(file_data.getvalue()).decode("utf-8")
+
+        # Create an attachment for the JSON file
+        attachment = self.env["ir.attachment"].create(
+            {
+                "name": "sale_order_json_report.json",
+                "type": "binary",
+                "datas": file_data_base64,
+                "res_model": "sale.order",
+                "res_id": self.id,
+                "mimetype": "application/json",
+            }
+        )
+
+        # Return the URL for the file to be downloaded
         return {
             "type": "ir.actions.act_url",
             "url": "/web/content/%d?download=true" % attachment.id,
